05898fefc5363b7e53f01f178977633635daf2a5
pedrohpe <pedroh.egler@hotmail.com>
2016-06-09 17:10:31 -0300

    2.4 inicio funcao manager

--- src/gerenciador.cpp
+++ src/gerenciador.cpp
@@ -170,3 +170,63 @@ man_ret editaTarefa(t_grafo *g, int IDMod, char tarefaNova[]){
 
     return MAN_OK;
 }
+
+man_ret startMan(){
+	inicializaInterface();
+	imprimeMenuNcurses();
+	//resetar a propriedade esta_concluida de todos os vertices
+	
+	
+	fechaInterface();
+}
+
+man_ret manager(char* nome_arq, int tempo){
+	t_grafo* g;
+	TipoLista *l_concluidas, *l_atual; //l_concluidas-> lista de tarefas ja concluidas e l_atual-> lista de tarefas ja iniciadas mas nao concluidas
+	t_vertix* v, v_sucessor;
+	t_item item_add, item_atual;
+	Celula* cel_atual, cel_sucessor, cel_ante;
+	int i,j;
+	
+	l_concluidas = criaLista();
+	l_atual = criaLista();
+	if (l_concluidas == NULL) return MAN_ERR;
+	if (l_atual == NULL) return MAN_ERR;
+	
+	g = leitura_arquivo(nome_arq);
+	
+	for(v = getOrigens(g); v != NULL; v = v->prox){
+		if(v->propriedades.inicio <= tempo){
+			if(v->propriedades.inicio + v->propriedades.duracao <= tempo){
+				item_add.ID = v->propriedades.ID;
+				item_add.peso = 0; //placeholder
+				if(insereLista(l_concluidas, item_add) == LISTA_ERR){
+					return MAN_ERR;
+				} else {
+					v->propriedades.esta_concluida = true;
+				} //utilizar lista de arestas (buscar vertix na l_vertices quando preciso) ou criar lista de vertices?
+			} else {
+				if(insereLista(l_atual, item_add) == LISTA_ERR){
+					return MAN_ERR;
+				}
+			}
+		}//if
+	}//for
+	
+	for(cel_atual = l_concluidas->inicio; cel_atual != NULL; cel_atual = cel_atual->prox){
+		v = buscaVertice(g, cel_atual->item.ID); //busca vertice no grafo com a ID igual a celula na lista de concluidos
+		if(v == NULL){
+			return MAN_ERR;
+		}
+		for(cel_sucessor = v->adjacentes->inicio; cel_sucessor != NULL; cel_sucessor  = cel_sucessor->prox){
+			item_atual = cel_sucessor->item;
+			v_sucessor = buscaVertice(g, item_atual.ID);
+			if(v == NULL){
+				return MAN_ERR;
+			}
+			for(cel_ante = v_sucessor->antecessores->inicio; cel_ante != NULL; cel_ante = cel_ante->prox){
+				
+			}//for antecessores			
+		}//for sucessores
+	}//for lista_concluidos
+}
